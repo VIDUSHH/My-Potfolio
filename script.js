b29e2cf2b64d03b484a3fcc56bd160757536dd77
VIDUSHH <vidushdabeer06@gmail.com>
2025-07-10 00:47:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ cancelBtn.onclick = () => {
 function downloadCV() {
   const link = document.createElement("a");
   link.href = "./CV.pdf";
-  link.download = "./CV.pdf";
+  link.download = "Vidush_Dabeer_CV.pdf";
   document.body.appendChild(link);
   link.click();
   document.body.removeChild(link);
