e77023b25f0d38e6b212636c126f1213b7e7ab18
VIDUSHH <vidushdabeer06@gmail.com>
2025-07-10 00:50:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,8 @@
-// Toggle menu (if implemented)
-const menuBtn = document.querySelector(".menu-btn");
-const cancelBtn = document.querySelector(".cancel-btn");
-const navMenu = document.querySelector(".menu");
-
-menuBtn.onclick = () => {
-  navMenu.classList.add("active");
-};
-
-cancelBtn.onclick = () => {
-  navMenu.classList.remove("active");
-};
-
-// Download CV
 function downloadCV() {
   const link = document.createElement("a");
-  link.href = "./CV.pdf";
+
+  link.href = "./CV.pdf"; 
+
   link.download = "Vidush_Dabeer_CV.pdf";
   document.body.appendChild(link);
   link.click();
